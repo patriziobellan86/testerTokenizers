8421e655a2b16e34ea06fd55e43a2cac907bc9e4
Patrizio Bellan <patrizio.bellan@gmail.com>
2016-03-29 15:22:05 +0200

    Added files via upload

--- Test.py
+++ Test.py
@@ -4,11 +4,15 @@ questo file avvia tutti i test, con differenti dimensioni di corpus e combinazio
 di tokenizer ed in fine analizza tutti i dati
 """
 
+from __future__ import division
+
 from Analizzatore import Analizzatore
 from TestTokenizer import TestTokenizer
 from DimSamplesPunkt import DimSamplesPunkt
+from Tools import Tools
 
 import sys
+import glob
 
 class Test ():
     r"""
@@ -19,20 +23,21 @@ class Test ():
         return u"vers.0.3.5.b"
     
 
-    def __init__ (self, dimCorpus, dimTrainingMyPunktTok):
+    def __init__ (self, dimCorpus):
         """
             dimTrainingMyPunktTok è espresso in numero di parole, viene trasformato in numero di sents da usare
             
         """
+        
         self.folderDati = u"dati\\"
         self.folderGrafici = self.folderDati + u"grafici\\"
         self.folderPdfs = self.folderDati + u"pdfs\\"
         self.folderPunkt = u"punkt\\"
         self.folderTestFiles = u"test files\\"
-        
-        self.dimCorpus = dimCorpus
+        self.tools = Tools (0)
+        self.dimCorpus = [DimSamplesPunkt().nSents (dim) for dim in dimCorpus]
         # dimTrainingMyPunktTok è espresso in numero di parole, viene trasformato in numero di sents da usare
-        self.dimMyPunktTok = [DimSamplesPunkt().nSents (dim) for dim in dimTrainingMyPunktTok]
+        #self.dimMyPunktTok = [DimSamplesPunkt().nSents (dim) for dim in dimTrainingMyPunktTok]
 
         self.AvviaTests ()
 
@@ -42,30 +47,80 @@ class Test ():
         self._DellAllFiles ()   
     
         #avvio tutti i test con i parametri impostati
-        print "sistemare bene params in ingresso!!!!"
         TestTokenizer(fileRisultati = "Risultati", save = True, 
-            dimTests = self.dimCorpus, aggiornaDatiTest = False, 
-            dimsTrainTok = self.dimMyPunktTok)
+            dimTests = self.dimCorpus, aggiornaDatiTest = False)
         
         print "Test creati con successo\navvio analisi dei dati"
         Analizzatore ()
 
+
     def _DellAllFiles (self):
         #self.tools.DelAllFiles (self.folderPunkt)
+        print "Eliminazione test files precendenti"
         self.tools.DelAllFiles (self.folderTestFiles)
+        print "Elilminazione grafici precedenti"
         self.tools.DelAllFiles (self.folderGrafici)
+        print "Eliminazione pdfs precedenti"
         self.tools.DelAllFiles (self.folderPdfs)
-  
+        print "Eliminazione file abbreviazioni precedenti"
+        for file in glob.glob (self.folderDati + '*.abl'):
+            self.tools.DelFile (file)
+        print "Eliminazione file stopwords precedenti"
+        for file in glob.glob (self.folderDati + '*.stopWords'):
+            self.tools.DelFile (file)
+            
     ########################################################################
         
 ###########################################################################    
-        
-if __name__ == '__main__':
+
+def AvvioConEstrazioneDaPaisa ():
+    import paisaSentsExtractor
+
+    print "eliminazione corpus precedente"    
+    #per prima cosa cancello i corpus precedenti
+    a=Tools(0)
+    
+    a.DelAllFiles ("corpus\\")
+    a.DelAllFiles ('corpus training\\')
+    
+    print "caricamento nuovo corpus"
+    #carico il nuovo corpus
+    nc = 20000
+    nt = 15000
+    
+    paisaSentsExtractor.PaisaSentsExtractor (nwords = (nc + nt), folderdst = "corpus\\", folderList = {nc : 'corpus training\\'})
+   
+    print "Avvio programma di TEST"
+    #avvio i tests  
+    #effettuo i test con dimensione nc, nc/2 e nc/4 che sono pari a:
+    # nc = 2000000
+    # nc/2 = 1000000
+    # nc/4 = 500000
+    Test (dimCorpus = [nc / 2, nc / 4, nc])
+    
+    
+def AvvioSenzaEstrazioneDaPaisa ():
+    #carico il nuovo corpus
+    nc = 20000
+    
+    print "Avvio programma di TEST"
+    #avvio i tests  
+    #effettuo i test con dimensione nc, nc/2 e nc/4 che sono pari a:
+    # nc = 2000000
+    # nc/2 = 1000000
+    # nc/4 = 500000
+    Test (dimCorpus = [nc / 2, nc / 4, nc])
+
+
+def TestMode ():
     print "test Mode"
     print
-    print "imposta i parametri che vuoi!!!!!"
+    print "dimensione dei test espressa in numero di parole. il numero differirà leggermente poichè approssimato alla fine della frase"
     
     dimCorpus = [1000, 2000]
-    dimTrainingMyPunktTok = [100000, 500000]
-    Tests (dimCorpus, dimTrainingMyPunktTok)
-        
+    
+    Test (dimCorpus)
+                
+if __name__ == '__main__':
+    #AvvioConEstrazioneDaPaisa ()
+    AvvioSenzaEstrazioneDaPaisa ()
